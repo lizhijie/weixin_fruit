dba6c6f8525e9bf18e09e37e4ffd82048cec770f
lizhijie <zhezhenglove@163.com>
2016-11-03 21:10:16 +0800

    2016.11.3 使用weui使网页显示与数据库对应,添加padding基本完成index.jsp

--- src/cn/bean/IndexBean.java
+++ src/cn/bean/IndexBean.java
@@ -1,8 +1,6 @@
 package cn.bean;
 
-
 import java.io.IOException;
-
 
 import java.util.ArrayList;
 
@@ -15,41 +13,56 @@ import org.json.JSONException;
 import cn.db.*;
 
 public class IndexBean extends Bean {
-	public IndexBean()
-	{
+	public IndexBean() {
 		super();
-		
+
 	}
+
 	String name;
+	ArrayList<AGoods> agoods;
+	String userId;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public ArrayList<AGoods> getAgoods() {
+		return agoods;
+	}
+
+	public void setAgoods(ArrayList<AGoods> agoods) {
+		this.agoods = agoods;
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-public void go(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, JSONException{	
-	this.setName(request.getParameter("li"));
-		String echostr = request.getParameter("echostr"); 
-		 if (echostr != null && echostr.length() > 1) {  
-	            request.getRequestDispatcher("/view/iden.jsp").forward(request, response);
-		
+
+	public void go(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException, JSONException {
+		this.setName(request.getParameter("li"));
+		String echostr = request.getParameter("echostr");
+		if (echostr != null && echostr.length() > 1) {
+			request.getRequestDispatcher("/view/iden.jsp").forward(request,
+					response);
+
+		}
+		ArrayList<AGoods> agoods = new ArrayList<AGoods>();
+		Data data = new Data();
+		data.getAllGoods(agoods);
+		String userid = login(request, response);
+		setAgoods(agoods);
+		setName(name);
+		setUserId(userid);
+		login(request, response);
+
 	}
-		 ArrayList<AGoods> agoods=new ArrayList<AGoods>();
-		 Data data=new Data();
-		 data.getAllGoods(agoods);
-		 String name="";
-		 int i;
-			for (i = 0; i < agoods.size() && agoods.get(i) != null; i++) {
-				name+="<tr><td>"+agoods.get(i).id + "</td><td>"
-						+ agoods.get(i).name + "</td><td>" + agoods.get(i).imgurl
-						+ "</td><td>" + agoods.get(i).about + "</td><td>"
-						+ agoods.get(i).price+"</td></tr>";
-			}
-			 String userid=login(request,response);
-			name="<table>"+name+"</table>userid:"+userid+"<br>";
-		 setName(name);
-		 login(request,response);
-		 
-	        
-}
 }

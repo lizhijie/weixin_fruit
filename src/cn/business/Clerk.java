de6a4e44cc65c81b80147893f7288e5e18f9d342
zhezheng <zhezheng@lidebian>
2017-05-29 19:04:12 +0800

    验证码支持随机数字

--- src/cn/business/Clerk.java
+++ src/cn/business/Clerk.java
@@ -12,7 +12,7 @@ public static int getRandom(int count) {
   return (int) Math.round(Math.random() * (count));
 }
 
-private static String string = "abcdefghijklmnopqrstuvwxyz"; 
+private static String string = "abcdefghijklmnopqrstuvwxyz0123456789"; 
 public static String getRandomString(int length){
     StringBuffer sb = new StringBuffer();
     int len = string.length();
